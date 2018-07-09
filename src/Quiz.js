11be0f1864281d3e7bc413d982c3cde9fa35173d
Angela Yang <ayang@kabbage.com>
2018-07-09 16:57:42 -0400

    next

--- src/Quiz.js
+++ src/Quiz.js
@@ -4,10 +4,16 @@ import QuizQuestion from './QuizQuestion.js';
 let quizData = require('./quiz_data.json'); 
 
 class Quiz extends Component {
+    constructor(props) {
+        super(props)
+        this.state = { quiz_position: 1 }
+    }
 
     render() {
         return (
-            <QuizQuestion />
+            <div>
+            <QuizQuestion quiz_question = quizData.quiz_questions[quiz_position-1] />
+            </div>
         );            
     }
 
